0b9c5d4d0399214a88da359e15c0c1071c954a94
Jeff Hoffman <jhoffman@trilogyed.com>
2018-01-21 22:48:48 +0000

    Created failing tests for frame scoring

--- server/src/models/Game.unit.test.js
+++ server/src/models/Game.unit.test.js
@@ -3,12 +3,33 @@
 import Game from "./Game";
 
 describe("Game", () => {
+  let g;
+  
   describe("when there have been two throws and no mark", () => {
     it("should calculate the correct score", () => {
-      const g = new Game();
+      g = new Game();
       g.add(5);
       g.add(4);
       expect(g.score()).toBe(9);
+    });
+  });
+  
+  describe("when there are four throws and no marks", () => {
+    beforeEach(() => {
+      g = new Game();
+      g.add(5);
+      g.add(4);
+      g.add(7);
+      g.add(2);
+    });
+    
+    it("should calculate the correct score", () => {
+      expect(g.score()).toBe(18);
+    });
+    
+    it("should calculate the correct score for each frame", () => {
+      expect(g.scoreForFrame(1)).toBe(9);
+      expect(g.scoreForFrame(2)).toBe(18);
     });
   });
 });
