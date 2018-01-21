2ff968f242c3ca0773f5e7f9d4aac2ad87d67f48
Jeff Hoffman <jhoffman@trilogyed.com>
2018-01-21 23:04:50 +0000

    Added scoreForFrame, tests are green

--- server/src/models/Game.js
+++ server/src/models/Game.js
@@ -1,14 +1,28 @@
 class Game {
   constructor() {
     this._score = 0;
+    this._throws = Array(21).fill(0);
+    this._currentThrow = 0;
   }
   
   add(pins) {
+    this._throws[this._currentThrow++] = pins;
     this._score += pins;
   }
   
   score() {
     return this._score;
+  }
+  
+  scoreForFrame(frame) {
+    let ball = 0;
+    let score = 0;
+    for (let currentFrame = 0; currentFrame < frame; currentFrame++) {
+      const firstThrow = this._throws[ball++];
+      const secondThrow = this._throws[ball++];
+      score += firstThrow + secondThrow;
+    }
+    return score;
   }
 }
 
